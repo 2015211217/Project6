7c4681d6a19afe35e791a5df0c469dcf6df278d1
JiangXuanke <15223370366@163.com>
2020-10-03 10:24:36 +0800

    The FashionMinst

--- ResNetModel.py
+++ ResNetModel.py
@@ -4,65 +4,131 @@ import torch.nn.functional as F
 
 
 # Define res-block
-class ResBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
-        super(ResBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, stride, padding=1, bias=False)
+# class ResBlock(nn.Module):
+#     def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+#         super(ResBlock, self).__init__()
+#         self.conv1 = nn.Conv2d(in_channels, out_channels, stride, padding=1, bias=False)
+#         self.bn1 = nn.BatchNorm2d(out_channels)
+#         self.relu = nn.ReLU(inplace=True)
+#         self.conv2 = nn.Conv2d(out_channels, out_channels, stride, padding=1, bias=False)
+#         self.bn2 = nn.BatchNorm2d(out_channels)
+#         self.downsample = downsample
+#
+#     def forward(self, input):
+#         residual = input
+#         x = self.conv1(input)
+#         x = self.bn1(x)
+#         x = self.relu(x)
+#         x = self.conv2(x)
+#         x = self.bn2(x)
+#
+#         output = self.relu(x)
+#         return output
+#
+#
+# class ResNet(nn.Module):
+#     def __init__(self, block, layers, num_classes=10):
+#         super(ResNet, self).__init__()
+#         self.in_channels = 8
+#         self.conv = nn.Conv2d(3, 8, 3, 1, 1)
+#         self.bn = nn.BatchNorm2d(8)
+#         self.relu = nn.ReLU(inplace=True)
+#         self.layer1 = self.make_layer(block, 8, layers[0])
+#         self.layer2 = self.make_layer(block, 16, layers[1])
+#         self.layer3 = self.make_layer(block, 32, layers[2])
+#         self.avg_pool = nn.AvgPool2d(kernel_size=8)
+#         self.fc = nn.Linear(8, num_classes)
+#
+#     def make_layer(self, block, out_channels, blocks, stride=1):
+#         downsample = None
+#         if (stride != 1) or (self.in_channels != out_channels):
+#             downsample = nn.Sequential(
+#                 nn.Conv2d(self.in_channels, out_channels, 3, stride=stride, padding=1),
+#                 nn.BatchNorm2d(out_channels)
+#             )
+#         layers = []
+#         layers.append(block(self.in_channels, out_channels, stride, downsample))
+#         self.in_channels = out_channels
+#         for i in range(1, blocks):
+#             layers.append(block(out_channels, out_channels))
+#         return nn.Sequential(*layers)
+#
+#     def forward(self, input):
+#         x = self.conv(input)
+#         x = self.bn(x)
+#         x = self.relu(x)
+#
+#         x = self.layer1(x)
+#         x = self.layer2(x)
+#         x = self.layer3(x)
+#
+#         x = self.avg_pool(x)
+#         x = x.view(x.size(0), -1)
+#         # x = self.fc(x)
+#         return x
+class GlobalAvgPool2d(nn.Module):
+    """
+    全局平均池化层
+    可通过将普通的平均池化的窗口形状设置成输入的高和宽实现
+    """
+
+    def __init__(self):
+        super(GlobalAvgPool2d, self).__init__()
+
+    def forward(self, x):
+        return F.avg_pool2d(x, kernel_size=x.size()[2:])
+
+
+class FlattenLayer(torch.nn.Module):
+    def __init__(self):
+        super(FlattenLayer, self).__init__()
+
+    def forward(self, x):  # x shape: (batch, *, *, ...)
+        return x.view(x.shape[0], -1)
+
+
+class Residual(nn.Module):
+    def __init__(self, in_channels, out_channels, use_1x1conv=False, stride=1):
+        """
+            use_1×1conv: 是否使用额外的1x1卷积层来修改通道数
+            stride: 卷积层的步幅, resnet使用步长为2的卷积来替代pooling的作用，是个很赞的idea
+        """
+        super(Residual, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, stride=stride)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+        if use_1x1conv:
+            self.conv3 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride)
+        else:
+            self.conv3 = None
         self.bn1 = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, stride, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(out_channels)
-        self.downsample = downsample
 
-    def forward(self, input):
-        residual = input
-        x = self.conv1(input)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.conv2(x)
-        x = self.bn2(x)
-
-        output = self.relu(x)
-        return output
+    def forward(self, X):
+        Y = F.relu(self.bn1(self.conv1(X)))
+        Y = self.bn2(self.conv2(Y))
+        if self.conv3:
+            X = self.conv3(X)
+        return F.relu(Y + X)
 
 
-class ResNet(nn.Module):
-    def __init__(self, block, layers, num_classes=2):
-        super(ResNet, self).__init__()
-        self.in_channels = 8
-        self.conv = nn.Conv2d(3, 8, 3, 1, 1)
-        self.bn = nn.BatchNorm2d(8)
-        self.relu = nn.ReLU(inplace=True)
-        self.layer1 = self.make_layer(block, 8, layers[0])
-        self.layer2 = self.make_layer(block, 16, layers[1])
-        self.layer3 = self.make_layer(block, 32, layers[2])
-        self.avg_pool = nn.AvgPool2d(kernel_size=8)
-        self.fc = nn.Linear(8, num_classes)
+def resnet_block(in_channels, out_channels, num_residuals, first_block=False):
+    '''
+    resnet block
+    num_residuals: 当前block包含多少个残差块
+    first_block: 是否为第一个block
+    一个resnet block由num_residuals个残差块组成
+    其中第一个残差块起到了通道数的转换和pooling的作用
+    后面的若干残差块就是完成正常的特征提取
+    '''
+    if first_block:
+        assert in_channels == out_channels  # 第一个模块的输出通道数同输入通道数一致
+    blk = []
+    for i in range(num_residuals):
+        if i == 0 and not first_block:
+            blk.append(Residual(in_channels, out_channels, use_1x1conv=True, stride=2))
+        else:
+            blk.append(Residual(out_channels, out_channels))
+    return nn.Sequential(*blk)
 
-    def make_layer(self, block, out_channels, blocks, stride=1):
-        downsample = None
-        if (stride != 1) or (self.in_channels != out_channels):
-            downsample = nn.Sequential(
-                nn.Conv2d(self.in_channels, out_channels, 3, stride=stride, padding=1),
-                nn.BatchNorm2d(out_channels)
-            )
-        layers = []
-        layers.append(block(self.in_channels, out_channels, stride, downsample))
-        self.in_channels = out_channels
-        for i in range(1, blocks):
-            layers.append(block(out_channels, out_channels))
-        return nn.Sequential(*layers)
 
-    def forward(self, input):
-        x = self.conv(input)
-        x = self.bn(x)
-        x = self.relu(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-
-        x = self.avg_pool(x)
-        x = x.view(x.size(0), -1)
-        # x = self.fc(x)
-        return x
+# 定义resnet模型结构
--- ResNetUsage.py
+++ ResNetUsage.py
@@ -1,75 +1,112 @@
-from ResNetModel import ResNet, ResBlock
-import torch.optim as optim
+from ResNetModel import GlobalAvgPool2d, Residual, resnet_block,FlattenLayer
+import time
 import torch.nn as nn
 import torch
 import torchvision
 from torchvision import datasets,transforms
-
-#就用resnet做了，弄了再说,tmd
+from PIL import Image
+#修改称能够显示accuraccy的样子，然后加入test，看test的分类
+#就用resnet做了，弄了再说
 EPOCHS = 10
 pre_epoch = 0
-BATCH_SIZE = 2048
+BATCH_SIZE = 16
 LR = 0.01
 data_train_dir = '/Users/jiangxuanke/Desktop/issm2020-ai-challenge/semTrain'
 # data_train_dir_notflaw = '/Users/jiangxuanke/Desktop/ISSM/issm2020-ai-challenge/semTrain'
-# data_test_dir = '/Users/jiangxuanke/Desktop/ISSM/issm2020-ai-challenge/semTest'
+data_test_dir = '/Users/jiangxuanke/Desktop/issm2020-ai-challenge/semTest'
 
 device = torch.device("cpu")
 
+normalize = transforms.Normalize(mean=[0.286], std=[0.352])#对像素值归一化
 transform_train = transforms.Compose([
     transforms.Pad(4),
     transforms.RandomHorizontalFlip(),
     transforms.RandomCrop(32),
     transforms.ToTensor(),
+    normalize
 ])
-# transform_test = transforms.Compose([
-#     transforms.ToTensor(),
-#     # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
-#     transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
-# ])
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    normalize
+])
 
 train_datasets = datasets.ImageFolder(data_train_dir, transform = transform_train)
-train_dataloader = torch.utils.data.DataLoader(train_datasets, batch_size=8, shuffle=True, num_workers=2)
+train_dataloader = torch.utils.data.DataLoader(train_datasets, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
 # test_datasets = datasets.ImageFolder(data_test_dir, transform = transform_test)
-# test_dataloader = torch.utils.data.DataLoader(test_datasets, batch_size=32, shuffle=True, num_workers=2)
-#
+# test_dataloader = torch.utils.data.DataLoader(test_datasets, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
 
-net = ResNet(ResBlock, [2, 2, 2]).to(device)
+# train_datasets = torchvision.datasets.FashionMNIST(root='../data', train=True, download=True, transform=transform_train)
+# train_dataloader = torch.utils.data.DataLoader(train_datasets, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
+# test_datasets = torchvision.datasets.FashionMNIST(root='../data', train=False, download=True, transform=transform_test)
+# test_dataloader = torch.utils.data.DataLoader(test_datasets, batch_size=100, shuffle=False, num_workers=2)
 
-#define loss funtion & optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=LR, momentum=0.9, weight_decay=5e-4)
+net = nn.Sequential(
+    nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),  # TODO: 缩小感受野, 缩channel
+    nn.BatchNorm2d(32),
+    nn.ReLU())
+# nn.ReLU(),
+# nn.MaxPool2d(kernel_size=2, stride=2))   # TODO：去掉maxpool缩小感受野
 
-def update_lr(optimizer, lr):
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
+# 然后是连续4个block
+net.add_module("resnet_block1", resnet_block(32, 32, 2, first_block=True))  # TODO: channel统一减半
+net.add_module("resnet_block2", resnet_block(32, 64, 2))
+net.add_module("resnet_block3", resnet_block(64, 128, 2))
+net.add_module("resnet_block4", resnet_block(128, 256, 2))
+# global average pooling
+net.add_module("global_avg_pool", GlobalAvgPool2d())
+# fc layer
+net.add_module("fc", nn.Sequential(FlattenLayer(), nn.Linear(256, 10)))
 
-# train net
-pre_epoch_total_step = len(train_dataloader)
-current_lr = LR
+def evaluate_accuracy(data_iter, net, device=None):
+	#评估模型在测试集的准确率
+    if device is None and isinstance(net, torch.nn.Module):
+        # 如果没指定device就使用net的device
+        device = list(net.parameters())[0].device
+    net.eval()
+    acc_sum, n = 0.0, 0
+    with torch.no_grad():
+        for X, y in data_iter:
+            acc_sum += (net(X.to(device)).argmax(dim=1) == y.to(device)).float().sum().cpu().item()
+            n += y.shape[0]
+    net.train()  # 改回训练模式
+    return acc_sum / n
 
-for epoch in range(EPOCHS):
-    print("EPOCH： ", 11-EPOCHS)
-    for i, (x, y) in enumerate(train_dataloader):
 
-        x = x.to(device)
-        y = y.to(device)
+def train_model(net, train_iter, batch_size, optimizer, device, num_epochs):
+    net = net.to(device)
+    print("training on ", device)
+    loss = torch.nn.CrossEntropyLoss()
+    for epoch in range(num_epochs):
+        train_l_sum, train_acc_sum, n, batch_count, start = 0.0, 0.0, 0, 0, time.time()
+        for X, y in train_iter:
+            X = X.to(device)
+            y = y.to(device)
+            y_hat = net(X)
+            l = loss(y_hat, y)
+            optimizer.zero_grad()
+            l.backward()
+            optimizer.step()
+            train_l_sum += l.cpu().item()
+            train_acc_sum += (y_hat.argmax(dim=1) == y).sum().cpu().item()
+            n += y.shape[0]
+            batch_count += 1
+        # test_acc = evaluate_accuracy(test_iter, net)
+        # 输出evaluate的结果
+        print('epoch %d, loss %.4f, train acc %.3f,  time %.1f sec'
+              % (epoch + 1, train_l_sum / batch_count, train_acc_sum / n,  time.time() - start))
+        # if test_acc > best_test_acc:
+        #     print('find best! save at model/best.pth')
+        #     best_test_acc = test_acc
+        #     torch.save(net.state_dict(), 'model/best.pth')
 
-        # forward
-        print('Processing')
-        prediction = net(x)
-        loss = criterion(prediction, y)
 
-        # backward
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+lr, num_epochs = 0.01, 10
+optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+batch_size = 64
+train_model(net, train_dataloader, batch_size, optimizer, device, num_epochs)
 
-        if (i+1) % 100 == 0:
-            template = r"Epoch:{}/{}, step:{}/{}, Loss:{:.6f}"
-            print(template.format(epoch+1, EPOCHS, i+1, pre_epoch_total_step, loss.item()))
 
-    # decay learning rate
-    if (epoch+1) % 20 == 0:
-        current_lr = current_lr/2
-        update_lr(optimizer, current_lr)
+
+
