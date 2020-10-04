a50ed4c61c2e579a6dc1eddc7a67fc638f28eefd
JiangXuanke <15223370366@163.com>
2020-10-04 21:46:46 +0800

    Edition 1.0

--- ResNetUsage.py
+++ ResNetUsage.py
@@ -1,112 +1,134 @@
-from ResNetModel import GlobalAvgPool2d, Residual, resnet_block,FlattenLayer
-import time
-import torch.nn as nn
 import torch
+import torch.nn as nn
+import torch.optim as optim
 import torchvision
-from torchvision import datasets,transforms
+import torchvision.transforms as transforms
+import argparse
+from ResNetModel import ResNet18
+from torch.autograd import Variable as V
+import csv
+import os
 from PIL import Image
-#修改称能够显示accuraccy的样子，然后加入test，看test的分类
-#就用resnet做了，弄了再说
-EPOCHS = 10
-pre_epoch = 0
-BATCH_SIZE = 16
-LR = 0.01
-data_train_dir = '/Users/jiangxuanke/Desktop/issm2020-ai-challenge/semTrain'
-# data_train_dir_notflaw = '/Users/jiangxuanke/Desktop/ISSM/issm2020-ai-challenge/semTrain'
-data_test_dir = '/Users/jiangxuanke/Desktop/issm2020-ai-challenge/semTest'
+import numpy as np
 
-device = torch.device("cpu")
+dir_train = "/Users/jiangxuanke/Desktop/issm2020-ai-challenge/semTrain"
+dir_test = "/Users/jiangxuanke/Desktop/issm2020-ai-challenge/semTest/"
+dir_model_save = "/Users/jiangxuanke/Desktop/issm2020-ai-challenge/semResModel"
+# 定义是否使用GPU
 
-normalize = transforms.Normalize(mean=[0.286], std=[0.352])#对像素值归一化
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# 参数设置,使得我们能够手动输入命令行参数，就是让风格变得和Linux命令行差不多
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser.add_argument('--outf', default='./model/', help='folder to output images and model checkpoints')  # 输出结果保存路径
+parser.add_argument('--net', default='./model/Resnet18.pth', help="path to net (to continue training)")  # 恢复训练时的模型路径
+args = parser.parse_args()
+
+# 超参数设置
+EPOCH = 20  # 遍历数据集次数
+pre_epoch = 0  # 定义已经遍历数据集的次数
+BATCH_SIZE = 128  # 批处理尺寸(batch_size)
+LR = 0.1  # 学习率
+
+# 准备数据集并预处理
 transform_train = transforms.Compose([
-    transforms.Pad(4),
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomCrop(32),
+    transforms.RandomCrop(32, padding=4),  # 先四周填充0，在吧图像随机裁剪成32*32
+    transforms.RandomHorizontalFlip(),  # 图像一半的概率翻转，一半的概率不翻转
     transforms.ToTensor(),
-    normalize
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),  # R,G,B每层的归一化用到的均值和方差
 ])
+
 transform_test = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),  # 先四周填充0，在吧图像随机裁剪成32*32
     transforms.ToTensor(),
-    # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
-    normalize
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-train_datasets = datasets.ImageFolder(data_train_dir, transform = transform_train)
-train_dataloader = torch.utils.data.DataLoader(train_datasets, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
-# test_datasets = datasets.ImageFolder(data_test_dir, transform = transform_test)
-# test_dataloader = torch.utils.data.DataLoader(test_datasets, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
-
-# train_datasets = torchvision.datasets.FashionMNIST(root='../data', train=True, download=True, transform=transform_train)
-# train_dataloader = torch.utils.data.DataLoader(train_datasets, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
-# test_datasets = torchvision.datasets.FashionMNIST(root='../data', train=False, download=True, transform=transform_test)
-# test_dataloader = torch.utils.data.DataLoader(test_datasets, batch_size=100, shuffle=False, num_workers=2)
-
-net = nn.Sequential(
-    nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),  # TODO: 缩小感受野, 缩channel
-    nn.BatchNorm2d(32),
-    nn.ReLU())
-# nn.ReLU(),
-# nn.MaxPool2d(kernel_size=2, stride=2))   # TODO：去掉maxpool缩小感受野
-
-# 然后是连续4个block
-net.add_module("resnet_block1", resnet_block(32, 32, 2, first_block=True))  # TODO: channel统一减半
-net.add_module("resnet_block2", resnet_block(32, 64, 2))
-net.add_module("resnet_block3", resnet_block(64, 128, 2))
-net.add_module("resnet_block4", resnet_block(128, 256, 2))
-# global average pooling
-net.add_module("global_avg_pool", GlobalAvgPool2d())
-# fc layer
-net.add_module("fc", nn.Sequential(FlattenLayer(), nn.Linear(256, 10)))
-
-def evaluate_accuracy(data_iter, net, device=None):
-	#评估模型在测试集的准确率
-    if device is None and isinstance(net, torch.nn.Module):
-        # 如果没指定device就使用net的device
-        device = list(net.parameters())[0].device
-    net.eval()
-    acc_sum, n = 0.0, 0
-    with torch.no_grad():
-        for X, y in data_iter:
-            acc_sum += (net(X.to(device)).argmax(dim=1) == y.to(device)).float().sum().cpu().item()
-            n += y.shape[0]
-    net.train()  # 改回训练模式
-    return acc_sum / n
+trainset = torchvision.datasets.ImageFolder(dir_train, transform=transform_train)  # 训练数据集
+train_dataloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True,
+                                          num_workers=2)  # 生成一个个batch进行批训练，组成batch的时候顺序打乱取
+#
+# testset = torchvision.datasets.ImageFolder(dir_test,transform=transform_test)
+# test_dataloader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE, shuffle=False, num_workers=2)
 
 
-def train_model(net, train_iter, batch_size, optimizer, device, num_epochs):
-    net = net.to(device)
-    print("training on ", device)
-    loss = torch.nn.CrossEntropyLoss()
-    for epoch in range(num_epochs):
-        train_l_sum, train_acc_sum, n, batch_count, start = 0.0, 0.0, 0, 0, time.time()
-        for X, y in train_iter:
-            X = X.to(device)
-            y = y.to(device)
-            y_hat = net(X)
-            l = loss(y_hat, y)
-            optimizer.zero_grad()
-            l.backward()
-            optimizer.step()
-            train_l_sum += l.cpu().item()
-            train_acc_sum += (y_hat.argmax(dim=1) == y).sum().cpu().item()
-            n += y.shape[0]
-            batch_count += 1
-        # test_acc = evaluate_accuracy(test_iter, net)
-        # 输出evaluate的结果
-        print('epoch %d, loss %.4f, train acc %.3f,  time %.1f sec'
-              % (epoch + 1, train_l_sum / batch_count, train_acc_sum / n,  time.time() - start))
-        # if test_acc > best_test_acc:
-        #     print('find best! save at model/best.pth')
-        #     best_test_acc = test_acc
-        #     torch.save(net.state_dict(), 'model/best.pth')
+# 模型定义-ResNet
+net = ResNet18().to(device)
+
+# 定义损失函数和优化方式
+criterion = nn.CrossEntropyLoss()  # 损失函数为交叉熵，多用于多分类问题
+optimizer = optim.SGD(net.parameters(), lr=LR, momentum=0.9,
+                      weight_decay=5e-4)  # 优化方式为mini-batch momentum-SGD，并采用L2正则化（权重衰减）
+
+# 训练
+if __name__ == "__main__":
+    print("Start Training, Resnet-18!")  # 定义遍历数据集的次数
+    with open("log.txt", "w")as f2:
+        for epoch in range(pre_epoch, EPOCH):
+            print('\nEpoch: %d' % (epoch + 1))
+            net.train()
+            sum_loss = 0.0
+            correct = 0.0
+            total = 0.0
+            train_accuracy = 0
+            for i, data in enumerate(train_dataloader, 0):
+                # 准备数据
+                length = len(train_dataloader)
+                inputs, labels = data
+                inputs, labels = inputs.to(device), labels.to(device)
+                optimizer.zero_grad()
+
+                # forward + backward
+                outputs = net(inputs)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
+
+                # 每训练1个batch打印一次loss和准确率
+                sum_loss += loss.item()
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += predicted.eq(labels.data).cpu().sum()
+                print('[epoch:%d, iter:%d] Loss: %.03f | Acc: %.3f%% '
+                      % (epoch + 1, (i + 1 + epoch * length), sum_loss / (i + 1), 100. * correct / total))
+                if 100. * correct / total > train_accuracy:
+                    torch.save(net.state_dict(), dir_model_save)
+                #   '%s/net_%03d.pth' % (args.outf, epoch + 1)
+                f2.write('%03d  %05d |Loss: %.03f | Acc: %.3f%% '
+                         % (epoch + 1, (i + 1 + epoch * length), sum_loss / (i + 1), 100. * correct / total))
+                f2.write('\n')
+                f2.flush()
+
+    net = ResNet18().to(device)  # 先定义net的结构
+    net.load_state_dict(torch.load(dir_model_save))
+    print("Starting Test!")
+    net = net.eval()
+
+    csvFile = open("/Users/jiangxuanke/Desktop/issm2020-ai-challenge/TestResult.csv", "w")  # 创建csv文件
+    writer = csv.writer(csvFile)
+    writer.writerow(["Id", "LABEL"])
+    results = []
+    image_number = 0
+    img_test = os.listdir(dir_test)
+    img_test.sort()
+    print(img_test)
+    for i in range(len(img_test)):
+        img = Image.open(dir_test + img_test[i])
+        # img = img.convert('RGB')
+
+        input = transform_test(img)
+        input = input.unsqueeze(0)  # 这里经过转换后输出的input格式是[C,H,W],网络输入还需要增加一维批量大小B
+
+        input = V(input)
 
 
-lr, num_epochs = 0.01, 10
-optimizer = torch.optim.Adam(net.parameters(), lr=lr)
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-batch_size = 64
-train_model(net, train_dataloader, batch_size, optimizer, device, num_epochs)
+        score = net(input)  # 将图片输入网络得到输出
 
-
-
+        probability = torch.nn.functional.softmax(score, dim=1)  # 计算softmax，即该图片属于各类的概率
+        max_value, index = torch.max(probability, 1)  # 找到最大概率对应的索引号，该图片即为该索引号对应的类别
+        image_number+=1
+        probability = np.round(probability.cpu().detach().numpy())
+        index = np.round(index.cpu().detach().numpy())
+        writer.writerow([image_number, index[0]+1])
+    csvFile.close()
 
